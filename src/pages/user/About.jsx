8dc16be6177b96dfde0764f64195dc87d5335c2e
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-20 20:47:20 +0530

    added useeffect about and contact page

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -4,6 +4,14 @@ import WheelSpinner from '../../components/shared/WheelSpinner'
 
 function About() {
   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       {loading ? (
--- src/pages/user/Contact.jsx
+++ src/pages/user/Contact.jsx
@@ -6,6 +6,13 @@ import WheelSpinner from '../../components/shared/WheelSpinner'
 
 function Contact() {
     const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div>
             {loading ? (
