9f6a42c3152cdc79bedb1fbe7bdbdeda18c62cc9
hadiyasameer <hadiyasameer2013@gmail.com>
2025-05-20 20:04:10 +0530

    updated cars.jsx to previous version

--- src/pages/user/Cars.jsx
+++ src/pages/user/Cars.jsx
@@ -36,7 +36,7 @@ function Cars() {
         loading ? (
         <WheelSpinner />
         ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
             {
               cars && cars.map((car, i) => (
                 <CarCards key={i} car={car} />
